cb118c47bac14a9c3ce5deb0dd4b4d30d819dffb
Maxgvn <46270859+maxgvn@users.noreply.github.com>
2019-04-28 22:45:58 +0200

    "old"

--- index.js
+++ index.js
@@ -25,17 +25,23 @@ db.once("open", function() {
 });
 
 const User = mongoose.model("User", {
-  name: String,
-  expensestotal: Number,
-  isDone: Boolean
+  name: { type: String, minlength: 1, maxlength: 20, required: true },
+  expensestotal: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Expense"
+  }
 });
 
 const Expense = mongoose.model("Expense", {
-  name: String,
-  isDone: Boolean
+  Description: { type: String, minlength: 2, maxlength: 20, required: true },
+  Amount: Number,
+  User: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
 });
 
-app.get("/users", async (req, res) => {
+app.get("/api/users", async (req, res) => {
   try {
     const Users = await User.find();
     res.json(Users);
@@ -44,12 +50,10 @@ app.get("/users", async (req, res) => {
   }
 });
 
-app.post("/usercreate", async (req, res) => {
+app.post("/api/usercreate", async (req, res) => {
   try {
     const newUser = new User({
-      name: req.body.name,
-      expensestotal: 0,
-      isDone: false
+      name: req.body.name
     });
 
     await newUser.save();
@@ -59,47 +63,46 @@ app.post("/usercreate", async (req, res) => {
   }
 });
 
-// app.post("/update", async (req, res) => {
-//   try {
-//     // 1) Récupérer l'objet à modifier depuis la base mongodb
-//     const task = await Task.findById(req.query.id);
-//     if (task) {
-//       // 2) Modifier l'objet
-//       if (req.body.name !== undefined) {
-//         task.name = req.body.name;
-//       }
-//       if (req.body.isDone !== undefined) {
-//         task.isDone = req.body.isDone;
-//       }
+app.get("/api/expenses", async (req, res) => {
+  try {
+    const Expenses = await Expense.find().populate("User");
+    res.json(Expenses);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-//       // 3) Sauvegarder l'objet modifié
-//       await task.save();
+app.post("/api/expensecreate", async (req, res) => {
+  try {
+    const newExpense = new Expense({
+      Description: req.body.description,
+      Amount: req.body.amount,
+      User: req.body.user
+    });
 
-//       res.json(task);
-//     } else {
-//       res.status(404).json({ message: "Task not found" });
-//     }
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// });
+    await newExpense.save();
+    res.json(newExpense);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-// app.post("/delete", async (req, res) => {
-//   try {
-//     // 1) Récupérer l'objet à modifier depuis la base mongodb
-//     const task = await Task.findById(req.query.id);
-//     if (task) {
-//       // 3) Supprimer l'objet modifié
-//       await task.remove();
-
-//       res.json(task);
-//     } else {
-//       res.status(404).json({ message: "Task not found" });
-//     }
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// });
+app.post("/api/deleteexpense", async (req, res) => {
+  console.log(req.query.id);
+  try {
+    const expensetodelete = await Expense.findById(req.query.id);
+    console.log(expensetodelete);
+    if (expensetodelete) {
+      await expensetodelete.remove();
+      console.log(expensetodelete);
+      res.json(expensetodelete);
+    } else {
+      res.status(404).json({ message: "Expense not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 app.all("*", (req, res) => {
   res.status(404).send("Page introuvable");
